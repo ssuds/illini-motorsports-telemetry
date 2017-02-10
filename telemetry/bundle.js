0cecf4f8c84790d3edf2febfb563e1f57dfec9f9
ssuds <ssuds@users.noreply.github.com>
2017-02-09 19:03:59 -0600

    implement base functionality to read in data from im-telemetry-spoof

---
+++ telemetry/bundle.js
@@ -0,0 +1,96 @@
+define([
+    'openmct',
+    './src/IMTelemetryServerAdapter',
+    './src/IMTelemetryInitializer',
+    './src/IMTelemetryModelProvider'
+], function (
+    openmct,
+    IMTelemetryServerAdapter,
+    IMTelemetryInitializer,
+    IMTelemetryModelProvider
+) {
+    openmct.legacyRegistry.register("illini-motorsports-telemetry/telemetry", {
+        "name": "Illini Motorsports Telemetry Adapter",
+        "extensions": {
+            "types": [
+                {
+                    "name": "Spacecraft",
+                    "key": "im.spacecraft",
+                    "cssclass": "icon-object"
+                },
+                {
+                    "name": "Subsystem",
+                    "key": "im.subsystem",
+                    "cssclass": "icon-object",
+                    "model": { "composition": [] }
+                },
+                {
+                    "name": "Measurement",
+                    "key": "im.measurement",
+                    "cssclass": "icon-telemetry",
+                    "model": { "telemetry": {} },
+                    "telemetry": {
+                        "source": "im.source",
+                        "domains": [
+                            {
+                                "name": "Time",
+                                "key": "timestamp"
+                            }
+                        ]
+                    }
+                }
+            ],
+            "roots": [
+                {
+                    "id": "im:sc",
+                    "priority": "preferred"
+                }
+            ],
+            "models": [
+                {
+                    "id": "im:sc",
+                    "model": {
+                        "type": "im.spacecraft",
+                        "name": "My Spacecraft",
+                        "location": "ROOT",
+                        "composition": []
+                    }
+                }
+            ],
+            "services": [
+                {
+                    "key": "im.adapter",
+                    "implementation": IMTelemetryServerAdapter,
+                    "depends": [ "$q", "IM_WS_URL" ]
+                }
+            ],
+            "constants": [
+                {
+                    "key": "IM_WS_URL",
+                    "priority": "fallback",
+                    "value": "ws://localhost:8081"
+                }
+            ],
+            "runs": [
+                {
+                    "implementation": IMTelemetryInitializer,
+                    "depends": [ "im.adapter", "objectService" ]
+                }
+            ],
+            "components": [
+                {
+                    "provides": "modelService",
+                    "type": "provider",
+                    "implementation": IMTelemetryModelProvider,
+                    "depends": [ "im.adapter", "$q" ]
+                },
+                {
+                    "provides": "telemetryService",
+                    "type": "provider",
+                    "implementation": "IMTelemetryProvider.js",
+                    "depends": [ "im.adapter", "$q" ]
+                }
+            ]
+        }
+    });
+});
